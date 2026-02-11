86841a258625cc8e00d3481c8100ae56e43a10e3
gzl163 <marusyagzl@gmail.com>
2026-02-11 20:36:42 +0700

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write(" Choose task: ");
+
+switch (Console.ReadLine())
+{
+    case "1":
+        int originalNumber;
+        int exponent;
+        int result;
+        int counter;
+
+        Console.Write(" Enter number a: ");
+        originalNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write(" Enter exponent: ");
+        exponent = Convert.ToInt32(Console.ReadLine());
+
+        result = 1;
